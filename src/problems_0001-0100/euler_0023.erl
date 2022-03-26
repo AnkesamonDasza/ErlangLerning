6a32fe2e1c208a1b7c3a43a5d4ab34d041daeb22
AnkesamonDasza <spdaszkiewicz@gmail.com>
2022-03-26 19:12:31 +0100

    Task 24

--- src/problems_0001-0100/euler_0023.erl
+++ src/problems_0001-0100/euler_0023.erl
@@ -5,36 +5,10 @@
 -export([run/0]).
 
 run() ->
-  AllNums = getAllNaturalNumbers(28123, []),
-  AbundantNumbers = getAllAbundantNums(28123, []),
-  SumsOfAbundNums = gestAbundandSums(AbundantNumbers),
-  Nums = AllNums -- SumsOfAbundNums,
-  io:fwrite("~w\n", [sumMembers(Nums, 0)]).
+  AllPerms = perms([0,1,2,3,4,5,6,7,8,9]),
+  Millionth = lists:nth(1000000, AllPerms),
+  io:fwrite("~w\n", [Millionth]).
 
-sumMembers([], Sum) -> Sum;
-sumMembers([H|T], Sum) ->
-  sumMembers(T, Sum+H).
-
-gestAbundandSums(Tab) ->
-  [X+Y || X <- Tab, Y <- Tab].
-
-getAllNaturalNumbers(0, Tab) -> Tab;
-getAllNaturalNumbers(Num, Tab) ->
-  getAllNaturalNumbers(Num-1, [Num|Tab]).
-
-getAllAbundantNums(1, Tab) -> Tab;
-getAllAbundantNums(Num, Tab) ->
-  Sum = getSumOfDivisors(Num),
-  if Sum > Num -> getAllAbundantNums(Num-1, [Num|Tab]);
-    true -> getAllAbundantNums(Num-1, Tab)
-  end.
-
-getSumOfDivisors(Num)->
-  getSumOfDivisors(Num, Num div 2,  0).
-getSumOfDivisors(_, 0, Sum) -> Sum;
-getSumOfDivisors(Num, Div, Sum) ->
-  IsDivided = (Num rem Div) == 0,
-  if
-    IsDivided -> getSumOfDivisors(Num, Div-1, Sum+Div);
-    true -> getSumOfDivisors(Num, Div-1, Sum)
-  end.
+perms([]) -> [[]];
+perms(L)  ->
+  [[H|T] || H <- L, T <- perms(L--[H])].
